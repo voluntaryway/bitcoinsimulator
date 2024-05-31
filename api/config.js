e0e1e08017392e70f3eea4d773fbfa3065275048
linx <voluntaryway@gmail.com>
2024-05-31 23:47:51 +0200

    wip

--- api/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // mongodb: 'mongodb://localhost/bitcoinsimulator'
-  mongodb: 'mongodb://satoshi:YGwtcGBAreOjmKFuC8s6I60qk4AcsYtA@130.61.25.26:27017/bitcoinsimulator?authSource=admin'
-};
